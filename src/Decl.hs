a218b5194932b2dc35ed992211387b4e191aa73c
zhxdmu <zhxdmu@nb-szum>
2024-02-18 21:32:39 +0300

    interim

--- src/Decl.hs
+++ src/Decl.hs
@@ -1,2 +1,11 @@
 
 module Decl where
+
+import qualified Data.Text as T
+
+type DeclBase = [T.Text]
+
+type DeclSynonym = (T.Text, T.Text, String)
+
+data Decl = Base DeclBase | Synonym DeclSynonym
+
