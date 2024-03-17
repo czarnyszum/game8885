433d234406bd01123abdf3da60a3f0c6eb2d9a0e
zhxdmu <zhxdmu@nb-szum>
2024-03-17 17:18:10 +0300

    interim

--- src/Species.hs
+++ src/Species.hs
@@ -40,12 +40,14 @@ data Tables b = Tables {
       _randGen     :: StdGen,
       _spAll       :: [Species b],
       _killRatio   :: Generator Bool,
+      _rapeRatio   :: Generator Bool,
       _space       :: Space b,
       _spColor     :: M.Map (Species b) T.Text,
       _actionGen   :: (Space b) -> Generator Action,
       _killGen     :: M.Map (Species b) ((Space b) -> Generator (Species b)),
       _fuckGen     :: M.Map (Species b) ((Space b) -> Generator (Species b)),
-      _sympathyGen :: M.Map (Species b, Species b) (Generator Bool)
+      _sympathyGen :: M.Map (Species b, Species b) (Generator Bool),
+      _creationGen :: M.Map (Species b, Species b) (Generator (Species b))
     }
 makeLenses ''Tables
 
@@ -61,6 +63,48 @@ newSpace =
           pop = M.fromList . map inj $ bs
           unoc = view freeSpace s
       return (Space unoc pop)
+
+chooseAction :: Ord b => Species b -> Space b -> SpaceCtx b (Space b)
+chooseAction x sp =
+    do
+      s <- gets (view space)
+      actG <- gets (view actionGen)
+      act <- sample randGen (actG s)
+      case act of
+        Kill -> killAction x sp
+        Fuck -> fuckAction x sp
+
+fuckAction :: Ord b => Species b -> Space b -> SpaceCtx b (Space b)
+fuckAction x sp =
+    do
+      current <- gets (view space)
+      fuckG <- gets (view fuckGen)
+      symG <- gets (view sympathyGen)
+      let
+          fg = (fuckG M.! x) current
+      y <- sample randGen fg
+      let
+          sym = symG M.! (x, y)
+      outcome <- sample randGen sym
+      if outcome
+      then creationAction x y sp
+      else rapeAction x y sp
+
+creationAction :: Ord b => Species b -> Species b -> Space b -> SpaceCtx b (Space b)
+creationAction x y sp =
+    do
+      cGen <- gets (view creationGen)
+      z <- sample randGen (cGen M.! (x, y))
+      let
+          occ = over freeSpace pred
+          currentUp = over population (M.adjust pred x . M.adjust pred y)
+          newUp = over population (M.adjust succ x . M.adjust succ y . M.adjust succ z)
+      modify (over space currentUp)
+      return (occ . newUp $ sp)
+
+rapeAction :: Ord b => Species b -> Species b -> Space b -> SpaceCtx b (Space b)
+rapeAction x y sp = undefined
+
 
 killAction :: Ord b => Species b -> Space b -> SpaceCtx b (Space b)
 killAction x sp =
